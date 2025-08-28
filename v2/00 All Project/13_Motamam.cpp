94c785ee33039b8872716706657eed18999286ad
Xrave.EXE <42661822+xrave77@users.noreply.github.com>
2025-08-28 07:21:51 +0330

    Add files via upload

---
+++ v2/00 All Project/13_Motamam.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int Satr, Soton;
+    cout << "Matrix chand dar chand bashad? ";
+    cin >> Satr >> Soton;
+
+    int ** Matrix = new int*[Satr];
+    for (int i = 0; i < Satr; i++)
+    {
+        cout << "Satr " << i + 1 << ": ";
+        Matrix[i] = new int [Soton];
+        for (int j = 0; j < Soton; j++)
+        {
+            cin >> Matrix[i][j];
+        }
+    }
+
+    for (int i = 0; i < Satr; i++)
+    {
+        for (int j = 0; j < Soton; j++)
+        {
+            cout << !Matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+// 13
+// ماتریس مجاورت را گرفته و ماتریس متمم آن در خروجی چاپ کنید
